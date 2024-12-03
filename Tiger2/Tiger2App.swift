0cb4bf96d416d4f5f38da7c8e62846e3f4fb3054
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-12-03 15:09:21 +0300

    Initial Commit

---
+++ Tiger2/Tiger2App.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger2App.swift
+//  Tiger2
+//
+//  Created by Роман on 03.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tiger2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
